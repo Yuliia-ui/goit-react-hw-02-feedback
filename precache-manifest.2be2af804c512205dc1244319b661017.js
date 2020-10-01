6655e3deb7d4ace6ee7dcdc773ea4112508674a1
Yuliia Kushnyrenko <kushnyrenkouliia8gmail.com>
2020-10-01 18:17:59 +0300

    Updates

---
+++ precache-manifest.2be2af804c512205dc1244319b661017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4d23043bff7e4187b12e28c957c35a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "cb6d802dddfb71864a5f",
+    "url": "/goit-react-hw-02-feedback/static/css/main.1693665f.chunk.css"
+  },
+  {
+    "revision": "bb20e201cda55ecc440e",
+    "url": "/goit-react-hw-02-feedback/static/js/2.14a99f68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.14a99f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6d802dddfb71864a5f",
+    "url": "/goit-react-hw-02-feedback/static/js/main.faeb5420.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
